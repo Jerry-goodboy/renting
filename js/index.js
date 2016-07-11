7df6be6e7648097b90c696a2f57419131b5f08dd
Jerry-goodboy <lvtong521@126.com>
2016-07-11 16:50:07 +0800

    change chart bar color

--- js/index.js
+++ js/index.js
@@ -4,6 +4,7 @@ function drawImgBlock (img, price, house) {
 
 }
 
+window.xDataArr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49];
 function chart4() {
 	var chart4 = echarts.init(document.getElementById('chart4'));
 	var option = {
@@ -32,21 +33,8 @@ function chart4() {
 	            splitLine: {
 					show:false
 	            },
-	            data : ["7", "10", "1", "8", "7", "8", "1", "6", "6", "9", "4", "5", "2", "2", "7", "9", "7", "2", "0", "6", "2", "4", "8", "5", "6", "4", "3", "1", "1", "1", "9", "9", "5", "10", "5", "10", "2", "7", "8", "0", "9", "6", "5", "7", "2", "8", "4", "3", "7", "5"]
-	            // function () {
-	            // 	var randArr = [];
-	            // 	for(var i=0;i<15;i++){
-	            // 		randArr.push(Math.round(Math.random()*10).toString());
-	            // 	}
-	            // 	for(var i=15;i<35;i++){
-	            // 		randArr.push(Math.round(Math.random()*10).toString());
-	            // 	}
-	            // 	for(var i=35;i<50;i++){
-	            // 		randArr.push(Math.round(Math.random()*10).toString());
-	            // 	}
-	            // 	return randArr;
-	            // }
-	        }
+	            data : window.xDataArr
+            }
 	    ],
 	    yAxis : [
 	        {
@@ -57,7 +45,7 @@ function chart4() {
 	    grid: {
 	    	borderWidth:0,
 	    	x:50,
-	    	y:50,
+	    	y:35,
 	    	x2:60,
 	    	y2:25
 	    },
@@ -70,23 +58,16 @@ function chart4() {
 	            barCategoryGap:0,
 	            itemStyle: {
 				    normal: {
-				        color:"#aaa"
+				        color:function(params){
+                            if((window.chartBarColorInx || []).indexOf(params.dataIndex) >= 0){
+                                return "#f00";
+                            }else {
+                                return "#aaa";
+                            }
+                        }
 				    }
 				},
-	            data:[0, 0, 0,  0, 0, 0, 0, 0, 3, 4, 7, 9,0, 0, 0, 0, 4, 8, 3, 8, 2, 8, 0, 0, 0, 8, 8, 0, 5,  1, 1, 0, 0, 0, 0, 0, 0,3, 1, 3, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-	            // function () {
-	            // 	var randArr = [];
-	            // 	for(var i=0;i<15;i++){
-	            // 		randArr.push(0);
-	            // 	}
-	            // 	for(var i=15;i<35;i++){
-	            // 		randArr.push(Math.round(Math.random()*10));
-	            // 	}
-	            // 	for(var i=35;i<50;i++){
-	            // 		randArr.push(0);
-	            // 	}
-	            // 	return randArr;
-	            // }
+	            data:window.seriesDataArr
 	        }
 	    ]
 	};
